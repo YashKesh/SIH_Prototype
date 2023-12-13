c2c45b3013f1dc26c3fc3171d1d1086d4b74a587
YashKesh <89805020+YashKesh@users.noreply.github.com>
2023-12-14 00:43:40 +0530

    nothing major

--- SIH/settings.py
+++ SIH/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6nsm)vsm7=+!+(&vmp$fcnyf#fxka03x4k(m6!0at+7czs+f)m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
